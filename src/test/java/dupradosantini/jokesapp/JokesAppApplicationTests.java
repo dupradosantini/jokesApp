8ced1bae6793d26dfdb118eca340fe2bff9e1080
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-02-04 10:59:50 -0300

    First Commit - Project Creation Closes #1

---
+++ src/test/java/dupradosantini/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dupradosantini.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
